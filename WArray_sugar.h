1f93aef188222ef1aaf87b1900e8df0ba08a38eb
Michael Nau <karl.weber33@gmail.com>
2017-05-05 15:30:48 +0200

    Renmoved unused macros from WArray_sugar.h in preparation for the actual usage of WArray_sugar.h

--- WArray_sugar.h
+++ WArray_sugar.h
@@ -5,29 +5,6 @@
 
 //------------------------------------------------------------
 
-#define warray_append( array, ... )											\
-({																			\
-	void* __elements[] = { __VA_ARGS__ };									\
-	warray_append_n( array, sizeof(__elements)/sizeof(void*), __elements );	\
-})
-
-#define warray_prepend( array, ... )											\
-({																			\
-	void* __elements[] = { __VA_ARGS__ };									\
-	warray_prepend_n( array, sizeof(__elements)/sizeof(void*), __elements );	\
-})
-
-#define warray_set( array, position, ... )											\
-({																					\
-	void* __elements[] = { __VA_ARGS__ };											\
-	warray_set_n( array, position, sizeof(__elements)/sizeof(void*), __elements );	\
-})
-
-#define warray_insert( array, position, ... )											\
-({																						\
-	void* __elements[] = { __VA_ARGS__ };												\
-	warray_insert_n( array, position, sizeof(__elements)/sizeof(void*), __elements );	\
-})
 
 //------------------------------------------------------------
 
